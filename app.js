8cb1a9bc4736dedb7adb0eeb170126ec4a8bea80
Thiago <thiago.spereira02@gmail.com>
2023-06-12 15:52:37 -0300

    removing body-parser (included on Express)

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
 
 var teste1 = require("./teste1");
@@ -8,14 +7,10 @@ var teste3 = require("./teste3");
 var teste4 = require("./teste4");
 var teste5 = require("./teste5");
 
-
 app.set('view engine', 'jade');
 
 app.use(express.json());
-app.use(express.urlencoded());
-
-app.use(bodyParser.json());                        
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -34,7 +29,6 @@ app.post("/users", teste2)
 app.delete("/users", teste3)
 app.put("/users", teste4)
 app.get("/users/access", teste5);
-
 
 const port  = 3000;
 app.listen(port, function(){
